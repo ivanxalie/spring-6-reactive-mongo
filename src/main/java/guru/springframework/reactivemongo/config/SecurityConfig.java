7036b9b0461f9de12ce6a51bef318bee601af4be
ivanxalie <ivanxalie@gmail.com>
2024-06-05 20:06:33 +0300

    Fix security. Add logbook

--- src/main/java/guru/springframework/reactivemongo/config/SecurityConfig.java
+++ src/main/java/guru/springframework/reactivemongo/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package guru.springframework.reactivemongo.config;
 import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
@@ -19,17 +18,9 @@ public class SecurityConfig {
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(CsrfSpec::disable)
+                .authorizeExchange(spec -> spec.matchers(EndpointRequest.toAnyEndpoint()).permitAll())
                 .authorizeExchange(spec -> spec.anyExchange().authenticated())
                 .oauth2ResourceServer(spec -> spec.jwt(withDefaults()))
-                .build();
-    }
-
-    @Bean
-    @Order(1)
-    public SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity security) throws Exception {
-        return security
-                .authorizeExchange(matcherRegistry -> matcherRegistry.matchers(EndpointRequest.toAnyEndpoint())
-                        .permitAll())
                 .build();
     }
 }
